3e26ff697cc08d0d9bb901cd3aed4727d768079a
Silent <tykhyi.andrii@gmail.com>
2019-03-16 01:04:11 +0200

    added basename

--- src/history.js
+++ src/history.js
@@ -1,3 +1,5 @@
 import createBrowserHistory from 'history/createBrowserHistory';
 
-export const history = createBrowserHistory()
+export const history = createBrowserHistory({
+  basename: process.env.PUBLIC_URL,
+})
