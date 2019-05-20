6cb6398d633c763ea085c83efa5cf93dd66186c3
Andrii Tykhyi <tykhyi.andrii@gmail.com>
2019-05-20 12:05:55 +0300

    added new server

--- src/actions/setData.js
+++ src/actions/setData.js
@@ -1,20 +1,20 @@
 import * as types from "../constants/actionTypes";
 import axios from "axios";
 import keyGenerator from "../utils/keyGenerator";
-import {history} from "../history"
+import { history } from "../history";
 
-export const setValue =  payload => ({
+export const setValue = payload => ({
   type: types.SET_VALUE,
   payload
 });
 
-export const setError =  error => ({
+export const setError = error => ({
   type: types.SET_ERROR,
   error
 });
 
-export const resetError =  error => ({
-  type: types.RESET_ERROR,
+export const resetError = error => ({
+  type: types.RESET_ERROR
 });
 
 export const resetState = () => ({
@@ -39,33 +39,34 @@ const hashRequestFail = error => ({
 export const getValueByHash = payload => async dispatch => {
   dispatch(hashRequest());
   try {
-	const { data } = await axios({
-	  method: "GET",
-	  url: "http://localhost:3005/data"
-	});
-	const dataByHash = data.filter(item => item.hash === payload);
-	if (dataByHash.length === 0) throw new Error("no hash in database");
-	localStorage.setItem("json", JSON.stringify(dataByHash[0].json, null, 4))
-	dispatch(hashRequestSucces(dataByHash[0]))
+    const { data } = await axios({
+      method: "GET",
+      url: "https://immense-dusk-23974.herokuapp.com/json-server"
+    });
+    const dataByHash = data.filter(item => item.hash === payload);
+    if (dataByHash.length === 0) throw new Error("no hash in database");
+    localStorage.setItem("json", JSON.stringify(dataByHash[0].json, null, 4));
+    dispatch(hashRequestSucces(dataByHash[0]));
   } catch (e) {
-	dispatch(hashRequestFail(e))
+    dispatch(hashRequestFail(e));
   }
-}
+};
 
 export const setValueByHash = payload => async dispatch => {
   dispatch(hashRequest());
   try {
-	const { data } = await axios({
-	  method: "POST",
-	  url: "http://localhost:3005/data",
-	  data: {
-		hash: keyGenerator(),
-		json: payload
-	  }
-	});
-	history.push(`/${data.hash}`)
-	dispatch(hashRequestSucces({json: data.json, hash: data.hash}))
+    const { data } = await axios({
+      method: "POST",
+      url: "https://immense-dusk-23974.herokuapp.com/json-server",
+      data: {
+        hash: keyGenerator(),
+        json: payload
+      }
+    });
+    const { hash, json } = data;
+    history.push(`/${hash}`);
+    dispatch(hashRequestSucces({ json, hash }));
   } catch (e) {
-	dispatch(hashRequestFail(e))
+    dispatch(hashRequestFail(e));
   }
-}
+};
